5fb34c5fd5714a1b9f1c8fd94c2347a183574c32
Joao Carreira <jfpcarreira@gmail.com>
2017-11-04 17:15:24 -0400

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule }      from '@angular/platform-browser';
+import { NgModule }           from '@angular/core';
+import { AngularFireModule }  from 'angularfire2';
+import { environment }        from '../environments/environment';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
-import { AppComponent } from './app.component';
+import { AppComponent }       from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+      BrowserModule
+    , AngularFireModule.initializeApp(environment.firebase, 'my-app-name') // imports firebase/app needed for everything
+    , AngularFirestoreModule // imports firebase/firestore, only needed for database features
+    , AngularFireAuthModule  // imports firebase/auth, only needed for auth features
   ],
   providers: [],
   bootstrap: [AppComponent]
